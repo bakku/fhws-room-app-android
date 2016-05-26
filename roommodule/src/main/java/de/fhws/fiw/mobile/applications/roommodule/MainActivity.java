26b7f118cb7fed8f23dbf4eaeb48fa906bd420ab
Müller, Patrick (BIN 6) <patrickbernd-michael.mueller@student.fhws.de>
2016-05-26 13:45:51 +0200

    Timetable. Layout Files added

--- roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/MainActivity.java
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/MainActivity.java
@@ -1,7 +1,10 @@
 package de.fhws.fiw.mobile.applications.roommodule;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button_to_timetable = (Button)findViewById(R.id.button_to_timetable);
+//        setOnClickListenerToTimetable(button_to_timetable);
+        button_to_timetable.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TimetableActivity.class);
+                startActivity(intent);
+            };
+        });
+    }
+
+    private void setOnClickListenerToTimetable(Button button_to_timetable){
+        button_to_timetable.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TimetableActivity.class);
+                startActivity(intent);
+            };
+        });
     }
 }
