1f374fd00b8486dfee70aa8690a5c28eb43adee7
student <theresamaria.hemm@student.fhws.de>
2016-06-09 12:36:09 +0200

    Initial Detail Activity

---
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/DetailActivity.java
@@ -0,0 +1,17 @@
+package de.fhws.fiw.mobile.applications.roommodule;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by student on 09.06.16.
+ */
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.timetable);
+
+    }
+}
