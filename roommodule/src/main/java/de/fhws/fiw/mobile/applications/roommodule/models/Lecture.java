33470167a78d450dd60241bd64f97582d46c7054
Christian Paling <christian.paling@googlemail.com>
2016-06-09 13:21:10 +0200

    implemented models

--- roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/models/Lecture.java
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/models/Lecture.java
@@ -1,7 +1,48 @@
 package de.fhws.fiw.mobile.applications.roommodule.models;
 
+import java.util.Date;
+
 /**
  * Created by christian on 09/06/16.
  */
 public class Lecture {
+
+    private String title;
+
+    private Date startOfLecture;
+
+    private Date endOfLecture;
+
+    public Lecture() {
+    }
+
+    public Lecture(String title, Date startOfLecture, Date endOfLecture) {
+        this.title = title;
+        this.startOfLecture = startOfLecture;
+        this.endOfLecture = endOfLecture;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getStartOfLecture() {
+        return startOfLecture;
+    }
+
+    public void setStartOfLecture(Date startOfLecture) {
+        this.startOfLecture = startOfLecture;
+    }
+
+    public Date getEndOfLecture() {
+        return endOfLecture;
+    }
+
+    public void setEndOfLecture(Date endOfLecture) {
+        this.endOfLecture = endOfLecture;
+    }
 }
--- roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/models/Room.java
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/models/Room.java
@@ -1,7 +1,38 @@
 package de.fhws.fiw.mobile.applications.roommodule.models;
 
+import java.util.List;
+
 /**
  * Created by christian on 09/06/16.
  */
 public class Room {
+
+    private String title;
+
+    private List<Lecture> listOfLectures;
+
+    public Room() {
+    }
+
+    public Room(String title, List<Lecture> listOfLectures) {
+        this.title = title;
+        this.listOfLectures = listOfLectures;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Lecture> getListOfLectures() {
+        return listOfLectures;
+    }
+
+    public void setListOfLectures(List<Lecture> listOfLectures) {
+        this.listOfLectures = listOfLectures;
+    }
+    
 }
