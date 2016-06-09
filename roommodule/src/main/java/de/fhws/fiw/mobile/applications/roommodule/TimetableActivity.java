d84dea1d308cb1b647de289fd4d4c4ae7efe09e4
Müller, Patrick (BIN 6) <patrickbernd-michael.mueller@student.fhws.de>
2016-06-09 12:45:01 +0200

    timetable ready

--- roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/TimetableActivity.java
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/TimetableActivity.java
@@ -1,7 +1,18 @@
 package de.fhws.fiw.mobile.applications.roommodule;
 
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by Patrick Müller on 26.05.2016.
@@ -13,5 +24,24 @@ public class TimetableActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timetable);
 
+        FrameLayout mainLayout = (FrameLayout) findViewById(R.id.timetable_framelayout_id);
+
+        View mView = new View(getBaseContext());
+        mView.setBackgroundColor(Color.BLACK);
+
+
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT,
+                FrameLayout.LayoutParams.WRAP_CONTENT);
+        mainLayout.addView(mView, params);
+
+        Log.d("TimetableActivity", "Die Farbe des Buttons: " + mView.getDrawingCacheBackgroundColor());
+
+        Resources r = getResources();
+        float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 40, r.getDisplayMetrics());
+
+
+        ViewGroup.LayoutParams layoutParamsOfView = mView.getLayoutParams();
+        layoutParamsOfView.height = (int) px;
+        layoutParamsOfView.width = (int) px;
     }
 }
