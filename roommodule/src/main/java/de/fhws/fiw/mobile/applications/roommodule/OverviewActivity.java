d6f1177a16e4dae6245ba89e11caa177de614a4f
KimJanek <kim-janek@web.de>
2016-06-09 15:56:24 +0200

    Merge branch 'master' of https://bitbucket.student.fiw.fhws.de:8443/scm/mbss16/roommanagement
    # Conflicts:
    #	roommodule/build.gradle
    Stuff 1.1

--- roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/OverviewActivity.java
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/OverviewActivity.java
@@ -1,7 +1,12 @@
 package de.fhws.fiw.mobile.applications.roommodule;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+
+import de.fhws.fiw.mobile.applications.roommodule.adapter.PagerAdapter;
+import de.fhws.fiw.mobile.applications.roommodule.transformer.DepthPageTransformer;
 
 /**
  * Created by Patrick Müller on 09.06.2016.
@@ -13,4 +18,18 @@ public class OverviewActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.timetable);
     }
+
+    private void initViewPagerAndTabs() {
+        ViewPager viewPager = (android.support.v4.view.ViewPager) findViewById(R.id.viewPager);
+        viewPager.setPageTransformer(true, new DepthPageTransformer());
+
+        PagerAdapter pagerAdapter = new PagerAdapter(getSupportFragmentManager());
+        //pagerAdapter.addFragment(new ProfessorFragment(), "Professors");
+        //pagerAdapter.addFragment(new UniversityPersonalFragment(), "Ordinary");
+        viewPager.setAdapter(pagerAdapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
 }
