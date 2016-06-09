6335049ebb9a10df29aa2e58f8ce751acfb95127
student <theresamaria.hemm@student.fhws.de>
2016-06-09 15:41:10 +0200

    First tab view test successful.

--- roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/DetailActivity.java
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/DetailActivity.java
@@ -2,16 +2,24 @@ package de.fhws.fiw.mobile.applications.roommodule;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by student on 09.06.16.
  */
 public class DetailActivity extends AppCompatActivity {
 
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.tabview);
+
+        //initViewPagerAndTabs();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("SupportActionBar");
 
     }
 }
