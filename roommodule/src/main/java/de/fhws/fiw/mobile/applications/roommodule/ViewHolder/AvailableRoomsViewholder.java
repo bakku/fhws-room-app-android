3fd472c20381992ef3bc0fd89d71b73ec42fbff7
KimJanek <kim-janek@web.de>
2016-06-10 15:57:38 +0200

    Still working on RecyclerView, Adapter and ViewHolder

--- roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/ViewHolder/AvailableRoomsViewholder.java
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/ViewHolder/AvailableRoomsViewholder.java
@@ -8,12 +8,17 @@ import de.fhws.fiw.mobile.applications.roommodule.models.Room;
 /**
  * Created by Kim on 10.06.2016.
  */
-public class AvailableRoomsViewholder extends RecyclerView.ViewHolder {
+public class AvailableRoomsViewholder extends RecyclerView.ViewHolder implements View.OnClickListener {
     public AvailableRoomsViewholder(View itemView) {
         super(itemView);
     }
 
     public void assignData(Room room){
         //TODO
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/ViewHolder/UsedRoomsViewHolder.java
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/ViewHolder/UsedRoomsViewHolder.java
@@ -8,12 +8,17 @@ import de.fhws.fiw.mobile.applications.roommodule.models.Room;
 /**
  * Created by Kim on 10.06.2016.
  */
-public class UsedRoomsViewHolder extends RecyclerView.ViewHolder {
+public class UsedRoomsViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     public UsedRoomsViewHolder(View itemView) {
         super(itemView);
     }
 
     public void assignData(Room room){
         //TODO
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
