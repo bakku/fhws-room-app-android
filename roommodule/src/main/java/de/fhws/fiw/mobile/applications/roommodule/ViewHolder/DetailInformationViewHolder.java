5473e978301ee215c8a8066e7902940094ee232d
student <theresamaria.hemm@student.fhws.de>
2016-06-16 13:27:50 +0200

    Created classes for detail information.

---
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/ViewHolder/DetailInformationViewHolder.java
@@ -0,0 +1,20 @@
+package de.fhws.fiw.mobile.applications.roommodule.ViewHolder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by student on 16.06.16.
+ */
+
+public class DetailInformationViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public DetailInformationViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
---
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/adapter/DetailInformationAdapter.java
@@ -0,0 +1,27 @@
+package de.fhws.fiw.mobile.applications.roommodule.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+import de.fhws.fiw.mobile.applications.roommodule.ViewHolder.DetailInformationViewHolder;
+
+/**
+ * Created by student on 16.06.16.
+ */
+
+public class DetailInformationAdapter extends RecyclerView.Adapter<DetailInformationViewHolder> {
+    @Override
+    public DetailInformationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(DetailInformationViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
