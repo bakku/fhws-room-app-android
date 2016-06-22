e8b295b1921275cbc04a72118620915c2ddb7556
Christian Paling <christian.paling@googlemail.com>
2016-06-22 21:50:05 +0200

    removed back button from nav

--- roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/activities/OverviewActivity.java
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/activities/OverviewActivity.java
@@ -3,11 +3,13 @@ package de.fhws.fiw.mobile.applications.roommodule.activities;
 import android.app.SearchManager;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.NavUtils;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.SearchView;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import de.fhws.fiw.mobile.applications.roommodule.R;
 import de.fhws.fiw.mobile.applications.roommodule.adapter.PagerAdapter;
@@ -17,15 +19,13 @@ import de.fhws.fiw.mobile.applications.roommodule.transformer.DepthPageTransform
 /**
  * Created by Patrick Müller on 09.06.2016.
  */
-public class OverviewActivity extends AppCompatActivity{
+public class OverviewActivity extends AppCompatActivity {
 
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tabview);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
