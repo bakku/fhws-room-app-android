4b68c320f6d03d5fb6101d2957d7a6ab02eb0c86
Müller, Patrick (BIN 6) <patrickbernd-michael.mueller@student.fhws.de>
2016-06-30 10:48:03 +0200

    refactoring of code

--- roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/activities/TimetableActivity.java
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/activities/TimetableActivity.java
@@ -51,6 +51,18 @@ public class TimetableActivity extends AppCompatActivity {
 
     }
 
+    private void setSizeOfAnQuarterHourInDp() {
+        int sizeOfQuarterHourInDp = (int) ((getResources().getDimension(R.dimen.timetable_grid_hour_size) /
+                getResources().getDisplayMetrics().density) / 4);
+        this.sizeOfAnQuarterHourInDp = sizeOfQuarterHourInDp;
+    }
+
+    private void setSizeOfFiveMinutesInDp() {
+        int sizeOfFiveMinutesInDp = (int) ((getResources().getDimension(R.dimen.timetable_grid_hour_size) /
+                getResources().getDisplayMetrics().density) / 12);
+        this.sizeOfFiveMinutesInDp = sizeOfFiveMinutesInDp;
+    }
+
     private void loadRoomData() {
         this.room = new RoomTestData().getTestRoom();
     }
@@ -65,130 +77,9 @@ public class TimetableActivity extends AppCompatActivity {
 
             addViewToTimetable(newView);
 
-            addMarginTopToView(newView, lecture);
+            setLayoutParametersOfTimetableEntry(newView, lecture);
 
         }
-    }
-
-    private int calculateMarginTop(Lecture lecture) {
-
-        int marginTopDp = 0;
-
-        int startHourOfTimetable = this.TIMETABLE_BEGINS_AT_HOUR;
-
-        int startHourOfLecture = TimeFormatter.getHourFromDate(lecture.getStartOfLecture());
-
-        int startMinutesOfLecture = TimeFormatter.getMinutesFromDate(lecture.getStartOfLecture());
-
-        int differenceInHours = calculateDifference(startHourOfLecture, startHourOfTimetable);
-
-        while (differenceInHours > 0) {
-            marginTopDp += this.sizeOfAnQuarterHourInDp * 4;
-            differenceInHours--;
-        }
-
-        while (startMinutesOfLecture > 0) {
-            marginTopDp += this.sizeOfAnQuarterHourInDp;
-            startMinutesOfLecture -= 15;
-        }
-
-        return marginTopDp;
-    }
-
-    private int calculateHeightOfTimeTableEntry(Lecture lecture){
-
-        int heightDp = 0;
-
-        int startHourOfLecture = TimeFormatter.getHourFromDate(lecture.getStartOfLecture());
-
-        int startMinutesOfLecture = TimeFormatter.getMinutesFromDate(lecture.getStartOfLecture());
-
-        int endHourOfLecture = TimeFormatter.getHourFromDate(lecture.getEndOfLecture());
-
-        int endMinutesOfLecture = TimeFormatter.getMinutesFromDate(lecture.getEndOfLecture());
-
-        while(!((startHourOfLecture == endHourOfLecture) && (startMinutesOfLecture == endMinutesOfLecture))){
-
-            startMinutesOfLecture = (startMinutesOfLecture + 15) % 60;
-
-            if(startMinutesOfLecture == 0){
-                startHourOfLecture++;
-            }
-
-            heightDp += this.sizeOfAnQuarterHourInDp;
-        }
-
-        return heightDp;
-    }
-
-    private int calculateDifference(int startHourOfLecture, int timetableBeginsAtHour) {
-        return startHourOfLecture - timetableBeginsAtHour;
-    }
-
-    private float getScreenWidthInDp() {
-        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
-        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
-
-        return dpWidth;
-    }
-
-    private void addMarginTopToView(View newView, Lecture lecture) {
-
-        int marginTopInPx = DpPixelConverter.dpToPixels(this, calculateMarginTop(lecture));
-
-        int widthInDp = (int) getScreenWidthInDp() - LEFT_MARGIN_OF_TIMETABLE_ENTRY_IN_DP - RIGHT_MARGIN_OF_TIMETABLE_ENTRY_IN_DP;
-        int widthInPx = DpPixelConverter.dpToPixels(this, widthInDp);
-
-        int heightInPx = DpPixelConverter.dpToPixels(this, calculateHeightOfTimeTableEntry(lecture) );
-
-        int leftMarginInPx = DpPixelConverter.dpToPixels(this, LEFT_MARGIN_OF_TIMETABLE_ENTRY_IN_DP);
-
-        FrameLayout.LayoutParams layoutParamsOfView = (FrameLayout.LayoutParams) newView.getLayoutParams();
-        layoutParamsOfView.leftMargin = leftMarginInPx;
-        layoutParamsOfView.height = heightInPx;
-        layoutParamsOfView.width = widthInPx;
-        layoutParamsOfView.gravity = Gravity.TOP;
-        layoutParamsOfView.topMargin = marginTopInPx;
-    }
-
-    private void addViewToTimetable(View newView) {
-        LinearLayout.LayoutParams layoutParams = createLinearLayoutParamsForNewView();
-
-        this.timetable_layout.addView(newView, layoutParams);
-    }
-
-    private void setSizeOfAnQuarterHourInDp() {
-        int sizeOfQuarterHourInDp = (int) ((getResources().getDimension(R.dimen.timetable_grid_hour_size) /
-                getResources().getDisplayMetrics().density) / 4);
-        this.sizeOfAnQuarterHourInDp = sizeOfQuarterHourInDp;
-    }
-
-    private void setSizeOfFiveMinutesInDp() {
-        int sizeOfFiveMinutesInDp = (int) ((getResources().getDimension(R.dimen.timetable_grid_hour_size) /
-                getResources().getDisplayMetrics().density) / 12);
-        this.sizeOfFiveMinutesInDp = sizeOfFiveMinutesInDp;
-    }
-
-    private LinearLayout createNewTimetableEntry(Lecture lecture) {
-
-        LinearLayout newView = new LinearLayout(getBaseContext());
-        newView.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
-        newView.setElevation(8);
-
-        TextView textView = new TextView(this);
-        textView.setText(lecture.getLectureName());
-        textView.setTextColor(Color.WHITE);
-
-        newView.addView(textView);
-
-        return newView;
-    }
-
-    private LinearLayout.LayoutParams createLinearLayoutParamsForNewView() {
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT);
-
-        return params;
     }
 
     private void displayCurrentTimeLine(){
@@ -198,6 +89,35 @@ public class TimetableActivity extends AppCompatActivity {
         addViewToTimetable(timelineLayout);
 
         setLayoutParametersOfTimeline(timelineLayout);
+    }
+
+    private LinearLayout createTimeline() {
+        LinearLayout newView = new LinearLayout(getBaseContext());
+        newView.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        newView.setElevation(8);
+        return newView;
+    }
+
+    private void addViewToTimetable(View newView) {
+        LinearLayout.LayoutParams layoutParams = createLinearLayoutParamsForNewView();
+
+        this.timetable_layout.addView(newView, layoutParams);
+    }
+
+    private LinearLayout.LayoutParams createLinearLayoutParamsForNewView() {
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        return params;
+    }
+
+    private void setLayoutParametersOfTimeline(LinearLayout timelineLayout){
+        int marginTopDp = calculateMarginTopOfTimeline();
+
+        FrameLayout.LayoutParams layoutParamsOfView = (FrameLayout.LayoutParams) timelineLayout.getLayoutParams();
+        layoutParamsOfView.height = DpPixelConverter.dpToPixels(this, 4);
+        layoutParamsOfView.width = DpPixelConverter.dpToPixels(this, (int) getScreenWidthInDp());
+        layoutParamsOfView.topMargin = DpPixelConverter.dpToPixels(this, marginTopDp);
     }
 
     private int calculateMarginTopOfTimeline(){
@@ -226,27 +146,99 @@ public class TimetableActivity extends AppCompatActivity {
         return marginTopDp;
     }
 
-    private LinearLayout createTimeline() {
+    private float getScreenWidthInDp() {
+        DisplayMetrics displayMetrics = this.getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+
+        return dpWidth;
+    }
+
+    private LinearLayout createNewTimetableEntry(Lecture lecture) {
+
         LinearLayout newView = new LinearLayout(getBaseContext());
-        newView.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+        newView.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
         newView.setElevation(8);
+
+        TextView textView = new TextView(this);
+        textView.setText(lecture.getLectureName());
+        textView.setTextColor(Color.WHITE);
+
+        newView.addView(textView);
+
         return newView;
     }
 
-    private void setLayoutParametersOfTimeline(LinearLayout timelineLayout){
-        int marginTopDp = calculateMarginTopOfTimeline();
+    private void setLayoutParametersOfTimetableEntry(View newView, Lecture lecture) {
 
-        FrameLayout.LayoutParams layoutParamsOfView = (FrameLayout.LayoutParams) timelineLayout.getLayoutParams();
-        layoutParamsOfView.height = DpPixelConverter.dpToPixels(this, 4);
-        layoutParamsOfView.width = DpPixelConverter.dpToPixels(this, (int) getScreenWidthInDp());
-        layoutParamsOfView.topMargin = DpPixelConverter.dpToPixels(this, marginTopDp);
+        int marginTopInPx = DpPixelConverter.dpToPixels(this, calculateMarginTopOfTimetableEntry(lecture));
+
+        int widthInDp = (int) getScreenWidthInDp() - LEFT_MARGIN_OF_TIMETABLE_ENTRY_IN_DP - RIGHT_MARGIN_OF_TIMETABLE_ENTRY_IN_DP;
+        int widthInPx = DpPixelConverter.dpToPixels(this, widthInDp);
+
+        int heightInPx = DpPixelConverter.dpToPixels(this, calculateHeightOfTimeTableEntry(lecture) );
+
+        int leftMarginInPx = DpPixelConverter.dpToPixels(this, LEFT_MARGIN_OF_TIMETABLE_ENTRY_IN_DP);
+
+        FrameLayout.LayoutParams layoutParamsOfView = (FrameLayout.LayoutParams) newView.getLayoutParams();
+        layoutParamsOfView.leftMargin = leftMarginInPx;
+        layoutParamsOfView.height = heightInPx;
+        layoutParamsOfView.width = widthInPx;
+        layoutParamsOfView.gravity = Gravity.TOP;
+        layoutParamsOfView.topMargin = marginTopInPx;
     }
 
-    private int getHourOfDate(Date date) {
-        return TimeFormatter.getHourFromDate(date);
+    private int calculateMarginTopOfTimetableEntry(Lecture lecture) {
+
+        int marginTopDp = 0;
+
+        int startHourOfTimetable = this.TIMETABLE_BEGINS_AT_HOUR;
+
+        int startHourOfLecture = TimeFormatter.getHourFromDate(lecture.getStartOfLecture());
+
+        int startMinutesOfLecture = TimeFormatter.getMinutesFromDate(lecture.getStartOfLecture());
+
+        int differenceInHours = calculateDifference(startHourOfLecture, startHourOfTimetable);
+
+        while (differenceInHours > 0) {
+            marginTopDp += this.sizeOfAnQuarterHourInDp * 4;
+            differenceInHours--;
+        }
+
+        while (startMinutesOfLecture > 0) {
+            marginTopDp += this.sizeOfAnQuarterHourInDp;
+            startMinutesOfLecture -= 15;
+        }
+
+        return marginTopDp;
     }
 
-    private int getMinutesOfDate(Date date) {
-        return TimeFormatter.getMinutesFromDate(date);
+    private int calculateDifference(int startHourOfLecture, int timetableBeginsAtHour) {
+        return startHourOfLecture - timetableBeginsAtHour;
+    }
+
+    private int calculateHeightOfTimeTableEntry(Lecture lecture){
+
+        int heightDp = 0;
+
+        int startHourOfLecture = TimeFormatter.getHourFromDate(lecture.getStartOfLecture());
+
+        int startMinutesOfLecture = TimeFormatter.getMinutesFromDate(lecture.getStartOfLecture());
+
+        int endHourOfLecture = TimeFormatter.getHourFromDate(lecture.getEndOfLecture());
+
+        int endMinutesOfLecture = TimeFormatter.getMinutesFromDate(lecture.getEndOfLecture());
+
+        while(!((startHourOfLecture == endHourOfLecture) && (startMinutesOfLecture == endMinutesOfLecture))){
+
+            startMinutesOfLecture = (startMinutesOfLecture + 15) % 60;
+
+            if(startMinutesOfLecture == 0){
+                startHourOfLecture++;
+            }
+
+            heightDp += this.sizeOfAnQuarterHourInDp;
+        }
+
+        return heightDp;
     }
 }
