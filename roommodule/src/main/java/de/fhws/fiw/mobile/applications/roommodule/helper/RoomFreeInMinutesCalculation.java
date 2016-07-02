001853f612390cabb576c5ae46bf713644514c26
Müller, Patrick (BIN 6) <patrickbernd-michael.mueller@student.fhws.de>
2016-07-02 14:26:40 +0200

    used rooms holder implemented

---
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/helper/RoomFreeInMinutesCalculation.java
@@ -0,0 +1,73 @@
+package de.fhws.fiw.mobile.applications.roommodule.helper;
+
+import java.util.Calendar;
+
+import de.fhws.fiw.mobile.applications.roommodule.models.Lecture;
+import de.fhws.fiw.mobile.applications.roommodule.models.Room;
+
+/**
+ * Created by Patrick Müller on 02.07.2016.
+ */
+public class RoomFreeInMinutesCalculation {
+
+    private Room room;
+
+    private Calendar currentTime;
+
+    public RoomFreeInMinutesCalculation(Room room){
+        this.room = room;
+        this.currentTime = TimeFormatter.getCurrentTimeAsCalendar();
+    }
+
+    public long calculateTimeRoomWillBeFreeInMinutes(){
+
+        long resultInMinutes = 0;
+
+        Lecture upcomingLecture = new UpcomingLectureDetermination(this.room).getUpcomingLecture();
+
+        Calendar startOfUpcomingLecture = Calendar.getInstance();
+        startOfUpcomingLecture.setTime(upcomingLecture.getStartOfLecture());
+
+        if(thereIsNoUpcomingLecture(upcomingLecture)){
+
+            resultInMinutes = timeUntilMidnightInMinutes();
+        }
+        else{
+
+            if (!this.room.roomIsFree()) {
+                //Dann müssen wir bis nächste Klausur warten, dass er frei wird
+
+                resultInMinutes = TimeFormatter.differenceInMinutes(this.currentTime, startOfUpcomingLecture);
+
+            } else {
+                //Dann ist der Raum sowieso schon frei
+
+                resultInMinutes = 0;
+            }
+        }
+
+        return resultInMinutes;
+    }
+
+    private boolean thereIsNoUpcomingLecture(Lecture lecture){
+
+        boolean result = false;
+
+        if(lecture == null){
+            result = true;
+        }
+
+        return result;
+    }
+
+    private long timeUntilMidnightInMinutes(){
+
+        Calendar todayMidnight = Calendar.getInstance();
+        todayMidnight.set(Calendar.HOUR_OF_DAY, 23);
+        todayMidnight.set(Calendar.MINUTE, 59);
+        todayMidnight.set(Calendar.SECOND, 59);
+        todayMidnight.set(Calendar.MILLISECOND, 0);
+
+        return TimeFormatter.differenceInMinutes(this.currentTime, todayMidnight);
+    }
+}
