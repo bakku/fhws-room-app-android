6fd55e91eb08c380d4c77798dc24599ab83e81b2
Christian Paling <christian.paling@googlemail.com>
2016-07-06 08:18:01 +0200

    implemented onclick listener

--- roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/viewholder/SearchViewHolder.java
+++ roommodule/src/main/java/de/fhws/fiw/mobile/applications/roommodule/viewholder/SearchViewHolder.java
@@ -1,6 +1,7 @@
 package de.fhws.fiw.mobile.applications.roommodule.viewholder;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
@@ -8,12 +9,13 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import de.fhws.fiw.mobile.applications.roommodule.R;
+import de.fhws.fiw.mobile.applications.roommodule.activities.DetailActivity;
 import de.fhws.fiw.mobile.applications.roommodule.models.Room;
 
 /**
  * Created by christian on 06/07/16.
  */
-public class SearchViewHolder extends RecyclerView.ViewHolder {
+public class SearchViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     private String nameOfRoomAsString;
     private final TextView nameOfRoom;
@@ -34,6 +36,7 @@ public class SearchViewHolder extends RecyclerView.ViewHolder {
         this.freeForValue = (TextView)itemView.findViewById(R.id.room_entry_free_for_value);
 
         this.context = itemView.getContext();
+        itemView.setOnClickListener(this);
     }
 
     public void assignData(Room room) {
@@ -47,4 +50,12 @@ public class SearchViewHolder extends RecyclerView.ViewHolder {
         this.nameOfRoomAsString = room.getRoomName();
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this.context, DetailActivity.class);
+
+        intent.putExtra("nameOfRoom", this.nameOfRoomAsString);
+
+        this.context.startActivity(intent);
+    }
 }
